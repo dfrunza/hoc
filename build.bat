20b8af29a8a0f29db379fc24c5625fa776ee985f
dfrunza <dumitru.frunza@gmail.com>
2017-06-24 20:56:19 -0400

    Revision of build.bat

--- build.bat
+++ build.bat
@@ -17,30 +17,26 @@ set L_flags=-incremental:no -opt:ref -subsystem:console
 
 set prog=test
 
-..\ctime cl /c %C_flags% ..\lib.c ..\lex.c ..\syntax.c /Fo
-..\ctime cl %C_flags% ..\hocc.c lib.obj lex.obj syntax.obj /link %L_flags%
+cl /c %C_flags% ..\lib.c ..\lex.c ..\syntax.c /Fo
+cl %C_flags% ..\hocc.c lib.obj lex.obj syntax.obj /link %L_flags%
 
 rem NOTE: The full path to the .hoc source is needed for Vim QuickFix to work properly.
-if %errorlevel% neq 0 goto :build_failed
-..\ctime.exe hocc.exe %cd%\..\%prog%.hoc > debug.txt
+if %errorlevel% neq 0 goto :end
+hocc.exe %cd%\..\%prog%.hoc > debug.txt
 
-rem if %errorlevel% neq 0 goto :hocc_exe_error
+rem if %errorlevel% neq 0 goto :hocc_error
 rem ..\ctime.exe cl %C_flags% ..\vm.c %prog%.res /link %L_flags% /Fe:%prog%.exe 
 
-rem echo build successful
 goto :end
 
-:build_failed
-echo build failed
-goto :end
-
-:hocc_exe_error
-echo hocc.exe exited with error code
+:hocc_error
+echo hocc.exe error
 goto :end
 
 :end
 popd
 
-cloc.exe lib.c hasm.h hasm.c lex.h lex.c syntax.h syntax.c typecheck.h typecheck.c ^
-runtime_obj.h runtime_obj.c codegen.c hocc.c vm.c semantic.c
+cloc.exe hasm.h lex.h lib.h runtime_obj.h syntax.h typecheck.h codegen.c hasm.c ^
+hocc.c lex.c lib.c runtime_obj.c semantic.c syntax.c typecheck.c vm.c
+
 

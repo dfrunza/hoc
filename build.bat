f487f572bb83d1613031fabba2bbd4729f6900e9
dfrunza <dumitru.frunza@gmail.com>
2018-01-28 20:16:56 -0500

    Improved build.bat files

--- build.bat
+++ build.bat
@@ -1,48 +1,69 @@
+::        MSVC
+:: --------------------
 @ECHO off
-REM        MSVC
-REM --------------------
+
+cl > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Setting up the MSVC environment
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86 > NUL 2>&1
+)
+cl > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO MSVC environment is not in working state
+  GOTO :early_exit
+)
+
+SETLOCAL
 
 SET base_dir=%cd%
-IF NOT EXIST .\bin mkdir .\bin
-PUSHD .\bin
-
 SET hoc_file=test
 SET hoc_dir=%base_dir%\hoc
 SET src_dir=%base_dir%\src
 
-REM /wd4706       - assignment within conditional expression
-REM /wd4013       - undefined function (missing forward declaration)
-REM /wd4211       - nonstandard extension used (related to wd4013)
-REM /wd4306       - 'type cast' : conversion from 'uint8' to 'int32 *' of greater size
-REM /wd4459       - declaration hides global declaration
-REM /wd4456       - declaration hides previous local declaration
-REM /FC           - display full path of source code files passed to cl.exe in diagnostic text
-REM /Fo:<path>    - compile to object file
-REM /Fe:<path>    - compile to executable
-REM /c            - compile without linking
-REM /EHa-         - disable exceptions
-REM /GR-          - disable RTTI
-REM /GS-          - disable buffer security check
-REM /W{n}         - warning output level (/W0 disable all warnings)
-REM /Wall         - display all warnings
+:: /wd4706       - assignment within conditional expression
+:: /wd4013       - undefined function (missing forward declaration)
+:: /wd4211       - nonstandard extension used (related to wd4013)
+:: /wd4306       - 'type cast' : conversion from 'uint8' to 'int32 *' of greater size
+:: /wd4459       - declaration hides global declaration
+:: /wd4456       - declaration hides previous local declaration
+:: /FC           - display full path of source code files passed to cl.exe in diagnostic text
+:: /Fo:<path>    - compile to object file
+:: /Fe:<path>    - compile to executable
+:: /c            - compile without linking
+:: /EHa-         - disable exceptions
+:: /GR-          - disable RTTI
+:: /GS-          - disable buffer security check
+:: /W{n}         - warning output level (/W0 disable all warnings)
+:: /Wall         - display all warnings
 
 SET C_flags=/Od /W4 /nologo /MTd /Zo /Zi /Gm- /GS- /GR- /EHa- /FC /D_CRT_SECURE_NO_WARNINGS ^
             /wd4201 /wd4127 /wd4100 /wd4706 /wd4211 /wd4306 /wd4459 /wd4456
 SET L_flags=/incremental:no /opt:ref /subsystem:console
 
-REM ..\ctime.exe ^
-cl %C_flags% %src_dir%\hocc.c /link %L_flags%
-IF %ERRORLEVEL% NEQ 0 GOTO :end
+IF NOT EXIST .\bin (
+  ECHO Creating the .\bin directory
+  mkdir .\bin
+)
+PUSHD .\bin
 
-REM NOTE: The full path to the .hoc source is needed in order for Vim QuickFix to work properly.
+cl %C_flags% %src_dir%\hocc.c /link %L_flags%
+
+IF %ERRORLEVEL% NEQ 0 (
+  GOTO :end
+)
+
+:: NOTE: The full path to the .hoc source is needed in order for Vim QuickFix to work properly.
+
 ECHO %hoc_file%.hoc
 hocc %hoc_dir%\%hoc_file%.hoc
-IF %ERRORLEVEL% NEQ 0 GOTO :end
+IF %ERRORLEVEL% NEQ 0 (
+  GOTO :end
+)
 
-REM /Cx     - preserve case in publics, externs
-REM /Zi     - add symbolic debug info
-REM /Fl     - generate listing
-REM /c      - assemble without linking
+:: /Cx     - preserve case in publics, externs
+:: /Zi     - add symbolic debug info
+:: /Fl     - generate listing
+:: /c      - assemble without linking
 ml /nologo /Cx /Zi /Fl %hoc_file%.asm ^
    /link /nologo /subsystem:console /incremental:no /entry:startup kernel32.lib
 
@@ -52,4 +73,5 @@ POPD
 cloc.exe %src_dir%\hocc.h %src_dir%\hocc.c %src_dir%\lib.c %src_dir%\platform.h %src_dir%\translate.c ^
   %src_dir%\lex.c %src_dir%\syntax.c  %src_dir%\sym.c %src_dir%\type.c %src_dir%\ir_gen.c %src_dir%\x86_gen.c
 
+:early_exit
 
--- gcc-build.bat
+++ gcc-build.bat
@@ -1,38 +1,69 @@
+::        GCC
+:: --------------------
 @ECHO off
-REM        GCC
-REM --------------------
+
+gcc -v > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Setting up the MINGW environment
+  SET "PATH=%PATH%;d:\mingw\bin\"
+)
+gcc -v > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO MINGW environment is not in working state
+  GOTO :early_exit
+)
+
+ml > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Setting up the MSVC environment
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86 > NUL 2>&1
+)
+ml > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO MSVC environment is not in working state
+)
+
+SETLOCAL
 
 SET base_dir=%cd%
-IF NOT EXIST .\bin mkdir .\bin
-PUSHD .\bin
-
-SET C_flags=-g -ggdb -Winline
-SET L_flags=
-
 SET hoc_file=test
 SET hoc_dir=%base_dir%\hoc
 SET src_dir=%base_dir%\src
 
-REM         Without CRT
-REM ---------------------------
-REM If you use -nostdlib, you get an unresolved reference to __main, since it's defined in the standard GCC library.
-REM Include -lgcc at the end of your compiler command line to resolve this reference.
-REM Calling __main is necessary, even when compiling C code, to allow linking C and C++ object code together.
-REM gcc -std=c99 -nostdlib -static %C_flags% %src_dir%\hocc.c -lkernel32 -lgcc -Wl,-e_mainCRTStartup -o hocc.exe
-REM ---------------------------
+::      CRT removal note
+:: ---------------------------
+:: If you use -nostdlib, you get an unresolved reference to __main, since it's defined in the standard GCC library.
+:: Include -lgcc at the end of your compiler command line to resolve this reference.
+:: Calling __main is necessary, even when compiling C code, to allow linking C and C++ object code together.
+:: gcc -std=c99 -nostdlib -static %C_flags% %src_dir%\hocc.c -lkernel32 -lgcc -Wl,-e_mainCRTStartup -o hocc.exe
+:: ---------------------------
 
-REM ..\ctime.exe ^
+SET C_flags=-g -ggdb -Winline
+SET L_flags=
+
+IF NOT EXIST .\bin (
+  ECHO Creating the .\bin directory
+  mkdir .\bin
+)
+PUSHD .\bin
+
 gcc -std=c99 %C_flags% %src_dir%\hocc.c -lkernel32 -o hocc.exe
-IF %errorlevel% NEQ 0 GOTO :end
 
-REM NOTE: The full path to the .hoc source is needed in order for Vim QuickFix to work properly.
+IF %ERRORLEVEL% NEQ 0 (
+  GOTO :end
+)
+
+:: NOTE: The full path to the .hoc source is needed in order for Vim QuickFix to work properly.
 ECHO %hoc_file%.hoc
 hocc %hoc_dir%\%hoc_file%.hoc > debug.txt
-IF %errorlevel% NEQ 0 GOTO :end
 
-REM /Cx     - preserve case in publics, externs
-REM /Zi     - add symbolic debug info
-REM /Fl     - generate listing
+IF %ERRORLEVEL% NEQ 0 (
+  GOTO :end
+)
+
+:: /Cx     - preserve case in publics, externs
+:: /Zi     - add symbolic debug info
+:: /Fl     - generate listing
 ml /nologo /Cx /Zi /Fl %hoc_file%.asm ^
    /link /nologo /subsystem:console /incremental:no /entry:startup kernel32.lib
 
@@ -41,4 +72,6 @@ POPD
 
 cloc.exe %src_dir%\hocc.h %src_dir%\hocc.c %src_dir%\lib.c %src_dir%\platform.h %src_dir%\translate.c ^
   %src_dir%\lex.c %src_dir%\syntax.c  %src_dir%\sym.c %src_dir%\type.c %src_dir%\ir_gen.c %src_dir%\x86_gen.c
+
+:early_exit
 
